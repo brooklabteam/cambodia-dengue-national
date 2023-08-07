a0a19a2b68e3f8d6f8e96063ad42f084e07f93cc
carabrook <cbrook04@gmail.com>
2023-08-07 10:51:01 -0500

    supplemental figures for Fig1

--- figure-development/Fig1-S1/archive/climate-case-lag-nat.R
+++ figure-development/Fig1-S1/archive/climate-case-lag-nat.R
@@ -9,7 +9,7 @@ library(dplyr)
 homewd = "/Users/carabrook/Developer/cambodia-dengue-national"
 
 #load the province-level transmission
-beta.df <- read.csv(file = paste0(homewd, "/data/beta_TSIR_fit_province.csv"), header = T, stringsAsFactors = F)
+beta.df <- read.csv(file = paste0(homewd, "/data/beta_TSIR_fit_national.csv"), header = T, stringsAsFactors = F)
 head(beta.df)
 
 
@@ -21,24 +21,10 @@ precip.dat <- read.csv(file = paste0(katwd, "/Climate_data/Cambodia_province_cli
 names(temp.dat)[names(temp.dat)=="adm1_name"] <- "provname"
 names(precip.dat)[names(precip.dat)=="adm1_name"] <- "provname"
 
-temp.dat = subset(temp.dat, provname!="Administrative unit not available")
-precip.dat = subset(precip.dat, provname!="Administrative unit not available")
+#temp.dat = subset(temp.dat, provname!="Administrative unit not available")
+#precip.dat = subset(precip.dat, provname!="Administrative unit not available")
 head(temp.dat) # year and biweek
 head(precip.dat) # year and biweek
-
-setdiff(unique(temp.dat$provname), unique(beta.df$provname)) #"Mondul Kiri"    "Oddar Meanchey" "Ratanak Kiri"   "Siemreap"       "Tboung Khmum"  
-temp.dat$provname[temp.dat$provname=="Siemreap"] <- "Siem Reap"
-temp.dat$provname[temp.dat$provname=="Oddar Meanchey"] <- "Otdar Meanchey"
-precip.dat$provname[precip.dat$provname=="Siemreap"] <- "Siem Reap"
-precip.dat$provname[precip.dat$provname=="Oddar Meanchey"] <- "Otdar Meanchey"
-
-#and plot to check
-temp.dat$year <-as.factor(temp.dat$year)
-
-ggplot(temp.dat) + geom_line(aes(x=biwk, y=temp_C, color=year)) + facet_wrap(provname~.)
-
-precip.dat$year <-as.factor(precip.dat$year)
-ggplot(precip.dat) + geom_line(aes(x=biwk, y=precip_mm, color=year)) + facet_wrap(provname~.)
 
 #need to correct precip.dat for 2020 and 2021 (even though will be dropped in merge)
 precip.dat$year <- as.numeric(as.character(precip.dat$year))
@@ -50,37 +36,37 @@ ggplot(precip.dat) + geom_line(aes(x=biwk, y=precip_mm, color=year)) + facet_wra
 
 #still some funky stuff in Kep, Pailin, Phnom Penh, Preah Sihanouk
 
-# and merge with case data
-temp.dat <- dplyr::select(temp.dat, -(X), -(temp_K))
-names(temp.dat)[names(temp.dat)=="biwk"] <- "biweek"
+#sum at national level
+temp.sum <- ddply(temp.dat, .(year, biwk), summarise, temp_C = mean(temp_C))
+precip.sum <- ddply(precip.dat, .(year, biwk), summarise, precip_mm = sum(precip_mm))
 
-precip.dat <- dplyr::select(precip.dat, -(X))
-names(precip.dat)[names(precip.dat)=="biwk"] <- "biweek"
+# and merge with case data
+names(temp.sum)[names(temp.sum)=="biwk"] <- "biweek"
+names(precip.sum)[names(precip.sum)=="biwk"] <- "biweek"
 
 head(beta.df)
 unique(beta.df$year)
+head(temp.sum)
+head(precip.sum)
 
-climate.merge <- merge(beta.df, temp.dat, by = c("provname", "year", "biweek"), all.x = T)
+climate.merge <- merge(beta.df, temp.sum, by = c("year", "biweek"), all.x = T)
 head(climate.merge)
-climate.merge <- merge(climate.merge, precip.dat, by = c("provname", "year", "biweek"), all.x = T)
+climate.merge <- merge(climate.merge, precip.sum, by = c("year", "biweek"), all.x = T)
 head(climate.merge)
 
 climate.merge$year <- as.factor(climate.merge$year)
 ggplot(climate.merge) + geom_line(aes(x=biweek, y=precip_mm, color=year)) + facet_wrap(provname~.)
 ggplot(climate.merge) + geom_line(aes(x=biweek, y=temp_C, color=year)) + facet_wrap(provname~.)
 
-#drop the sus reconstruction info
-climate.merge <- dplyr::select(climate.merge, -(rsquared), -(sus_reconstruction))
-
 #save this
 
-write.csv(climate.merge, file = paste0(homewd, "/data/climate_beta_prov.csv"), row.names = F)
+write.csv(climate.merge, file = paste0(homewd, "/data/climate_beta_nat.csv"), row.names = F)
 
-# and test for lags now - split by province first
+# and test for lags now 
 
-climate.split <- dlply(climate.merge, .(provname, epiyr))
+#split by epiyear
+climate.split <- dlply(climate.merge, .(epiyr))
 
-# Look for cross-correlations between 
 # Look for cross-correlations between 
 find.lags <- function(dat){
   
@@ -118,19 +104,20 @@ find.lags <- function(dat){
   
 }
 
-
 lag.climate.out <- lapply(climate.split, find.lags)
+
 lag.climate.df <- data.table::rbindlist(lag.climate.out)
 
-ddply(lag.climate.df, .(variable), summarise, mean_lag=mean(lag), median_lag = median(lag)) 
-#mean precip = -5, temp = -6
-#median precip = -2, temp = -7
-
 #and save lag data
-write.csv(lag.climate.df, file=paste0(homewd,"/data/lags_climate_prov.csv"), row.names = F)
+write.csv(lag.climate.df, file=paste0(homewd,"/data/lags_climate_nat.csv"), row.names = F)
 
+#now make shifted dataset based on lags and save
 
-#make lagged data based on optimal lags
+ddply(lag.climate.df, .(variable), summarise, mean_lag=mean(lag), median_lag = median(lag)) 
+#median lag for precip at national level is 2 biweeks (4 weeks) and for temp is 5 biweeks (10 weeks)
+#for province, this was 2 and 7
+
+#this is consistent with wagner. use it!
 
 #make shifted dataset and save
 head(climate.merge)
@@ -143,5 +130,7 @@ head(climate.shift)
 climate.shift$precip_mm_lag <- climate.merge$precip_mm[6:(length(climate.merge$precip_mm)-2)]
 
 #write data and do lagged regression in another script
-write.csv(climate.shift, file = paste0(homewd, "/data/lagged-prov-clim-beta.csv"), row.names = F)
+write.csv(climate.shift, file = paste0(homewd, "/data/lagged-nat-clim-beta.csv"), row.names = F)
+
+
 
