296114e9e95bb9fb9e68d4d7e8e3e202bd2b1b47
carabrook <cbrook04@gmail.com>
2024-04-23 10:43:48 -0500

    beast updates

---
+++ BEAST-tree/denv-beast/beast-out/denv2/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=denv2
+#SBATCH --account=pi-cbrook
+#SBATCH --partition=broadwl
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks=8
+#SBATCH --time=36:00:00
+
+module load midway2
+module load java/1.8
+module load cuda/8.0
+module load beagle/trunk
+module load beast/2.6.2
+
+/home/cbrook/beast/bin/beast -beagle_CPU -seed 777 /home/cbrook/denv-beast/denv2/denv2.xml
